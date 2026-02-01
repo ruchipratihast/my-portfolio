e5238353889a24c8935f090c947b191cf3447e19
Ruchi Pratihast <132348008+ruchipratihast@users.noreply.github.com>
2026-02-01 20:32:16 +0530

    Content and Readme Changes (#1)
    * content branch
    
    * images issue fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'export',
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
+  // Set assetPrefix and basePath to your repository name for production
+  assetPrefix: isProd ? '/my-portfolio/' : '',
+  basePath: isProd ? '/my-portfolio' : '',
+
 };
 
 export default nextConfig;
